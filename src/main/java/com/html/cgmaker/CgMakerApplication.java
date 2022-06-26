7992908c7b8068de52bc825a2b3fe061b26b00c0
dongyoung <dong5854@gmail.com>
2022-06-27 01:23:47 +0900

    first commit

---
+++ src/main/java/com/html/cgmaker/CgMakerApplication.java
@@ -0,0 +1,13 @@
+package com.html.cgmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/html/cgmaker/CgMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.html.cgmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
