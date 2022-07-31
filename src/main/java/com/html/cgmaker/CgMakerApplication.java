df53179bd3d6388c4d6c00329ec08060eb39d1ec
jw0293 <k38836919@gmail.com>
2022-07-31 13:24:15 +0900

    Refactor project

--- src/main/java/com/html/cgmaker/CgMakerApplication.java
+++ src/main/java/com/html/cgmaker/CgMakerApplication.java
@@ -2,8 +2,10 @@ package com.html.cgmaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CgMakerApplication {
 
 	public static void main(String[] args) {
