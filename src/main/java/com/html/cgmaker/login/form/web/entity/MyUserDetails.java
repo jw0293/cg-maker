d69827f7f620a5376d9dcfcceff328d4684a78e3
jw0293 <k38836919@gmail.com>
2022-08-10 16:50:11 +0900

    feat : add UserDetails

---
+++ src/main/java/com/html/cgmaker/login/form/web/entity/MyUserDetails.java
@@ -0,0 +1,44 @@
+package com.html.cgmaker.login.form.web.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Delegate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+@Getter
+public class MyUserDetails implements UserDetails {
+
+    @Delegate
+    private final Member member;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return member.getIsEnable();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return member.getIsEnable();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return member.getIsEnable();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return member.getIsEnable();
+    }
+}
