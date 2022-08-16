435925789708688e5b076b8f82d54007b0ac1ed6
jw0293 <k38836919@gmail.com>
2022-08-16 16:36:06 +0900

    feat : Refactor token handler

--- src/main/java/com/html/cgmaker/login/form/handler/CustomFormLoginSuccessHandler.java
+++ src/main/java/com/html/cgmaker/login/form/handler/CustomFormLoginSuccessHandler.java
@@ -3,6 +3,7 @@ package com.html.cgmaker.login.form.handler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.html.cgmaker.login.domain.constants.AuthConstants;
 import com.html.cgmaker.login.domain.dto.Token;
+import com.html.cgmaker.login.domain.enums.UserRole;
 import com.html.cgmaker.login.utils.TokenUtils;
 import com.html.cgmaker.login.form.web.entity.Member;
 import com.html.cgmaker.login.form.web.entity.MyUserDetails;
@@ -27,8 +28,11 @@ public class CustomFormLoginSuccessHandler extends SavedRequestAwareAuthenticati
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
         final Member member = ((MyUserDetails) authentication.getPrincipal()).getMember();
 
-        final Token token = tokenUtils.generateToken(member.getEmail(), "USER");
+        final Token token = tokenUtils.generateToken(member.getEmail(), UserRole.USER.getKey());
         response.addHeader(AuthConstants.AUTH_HEADER, AuthConstants.TOKEN_TYPE + " " + token.getAccessToken());
         response.addHeader(AuthConstants.REFRESH_HEADER, AuthConstants.TOKEN_TYPE + " " + token.getRefreshToken());
+
+        String tkn = new ObjectMapper().writeValueAsString(token);
+        log.info("Token = {}", tkn);
     }
 }
