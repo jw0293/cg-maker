15cece5f774f7c46ab8bd685f21d05901f2bd886
jw0293 <k38836919@gmail.com>
2022-08-18 13:50:37 +0900

    feat : Add RedisUtils

--- src/main/java/com/html/cgmaker/login/utils/RedisUtils.java
+++ src/main/java/com/html/cgmaker/login/utils/RedisUtils.java
@@ -3,12 +3,13 @@ package com.html.cgmaker.login.utils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 
 @RequiredArgsConstructor
-@Service
+@Component
 public class RedisUtils {
 
     private final StringRedisTemplate stringRedisTemplate;
